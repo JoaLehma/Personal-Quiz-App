6ea78747d89456aa0020355c7d54f1ba3c273ade
Joachim Lehmann <joa.lehmann@web.de>
2022-07-05 20:56:24 +0200

    Update Answer and Bookmark Button

--- showanswer.js
+++ showanswer.js
@@ -1,3 +1,7 @@
+/* Show Answer Button */
+
+console.clear();
+
 let button = document.querySelector('[data-js="button"]');
 let text = document.querySelector('[data-js="answer"]');
 
@@ -6,3 +10,25 @@ function onButtonClick() {
 }
 
 button.addEventListener("click", onButtonClick);
+
+button.addEventListener("click", function () {
+  /*
+  Update the text of the button to toggle beween "More" and "Less" when clicked
+  */
+  if (button.innerText.toLowerCase() === "hide answer") {
+    button.innerText = "Show answer";
+  } else {
+    button.innerText = "Hide Answer";
+  }
+});
+
+/* Bookmark Button */
+
+const bookmarkButton = document.querySelector('[data-js="bookmark"]');
+const bookmarkColor = document.querySelector('[data-js="bookmark--color"]');
+
+function onBookmarkClick() {
+  bookmarkColor.classList.toggle("ion_icon--color");
+}
+
+bookmarkButton.addEventListener("click", onBookmarkClick);
