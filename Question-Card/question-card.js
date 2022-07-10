7d445e08133d021ee74db0a007f5ba5a622efe53
Joachim Lehmann <joa.lehmann@web.de>
2022-07-10 20:51:08 +0200

    Add Answer and  Bookmark Button in Js

--- Question-Card/question-card.js
+++ Question-Card/question-card.js
@@ -28,14 +28,20 @@ export default function CreateCards() {
     container.classList.add("question__card");
     container.setAttribute("data-js", "cardContainer");
     main.append(container);
+
     /* Bookmark */
     const bookmark = document.createElement("button");
-    bookmark.classList.add("bookmark__button");
+    bookmark.classList.add("bookmark__button", "hover--effekt");
+    bookmark.innerHTML = `
+    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+        <path fill="currentColor" d="M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5C19,3.89 18.1,3 17,3Z" />
+    </svg>
+    `;
+    bookmark.addEventListener("click", () => {
+      bookmark.classList.toggle("bookmark__button--active");
+    });
     container.append(bookmark);
-    const icon = document.createElement("p");
-    icon.classList.add("ion_icon--color");
-    icon.innerHTML = "";
-    bookmark.append(icon);
+
     /* Title */
     const question = document.createElement("h2");
     question.classList.add("question__headline");
--- bookmark/bookmark.js
+++ bookmark/bookmark.js
@@ -1,8 +1,7 @@
 export default function Bookmark() {
-  const bookmarkButton = document.querySelector('[data-js="bookmark"]');
-  const bookmarkColor = document.querySelector('[data-js="bookmark--color"]');
-
-  function onBookmarkClick() {
-    bookmarkColor.classList.toggle("ion_icon--color");
-  }
+  // const bookmarkButton = document.querySelector('[data-js="bookmark"]');
+  // const bookmarkColor = document.querySelector('[data-js="bookmark--color"]');
+  // function onBookmarkClick() {
+  //   bookmarkColor.classList.toggle("ion_icon--color");
+  // }
 }
